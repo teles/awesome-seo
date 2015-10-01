c57d26c9c6f110e6fac714e71d428af8beeeb95d
teles <josetelesmaciel@gmail.com>
2015-09-30 23:47:13 -0300

    Adicionando bandeira do Brasil em conteúdos em português

--- run.js
+++ run.js
@@ -7,6 +7,16 @@ var http = require("http");
 var handlebars = require('handlebars');
 var fs = require('fs');
 
+handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
+
 var request = http.get(GLOBAL.endpoint_url, function (response) {
     var buffer = "";
     var data;
--- template.md
+++ template.md
@@ -1,10 +1,11 @@
 # Awesome SEO
     A curated list of seo links!
+[br]: https://upload.wikimedia.org/wikipedia/commons/4/40/Icons-flag-br.png  "Conteúdo disponível em português"
 
 {{#each this}}    
 ## {{@key}}
     {{#each this}}
-* [{{link.title}}]({{link.href}}) {{#if link.description}}- {{link.description}} {{/if}}    
+* [{{link.title}}]({{link.href}}) {{#if link.description}}- {{link.description}} {{/if}} {{#equal link.language 'Português'}}![brazilian flag][br]{{/equal}}
     {{/each}}
     
 {{/each}}
