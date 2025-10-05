2507e6d985b4d4d127775ae30706441f529e2975
José Teles <josetelesmaciel@gmail.com>
2025-10-05 18:52:43 -0300

    build(config): update Jest config for React testing
    - Add .test.tsx to testMatch pattern for React component tests
    - Add .tsx to coverage collection
    - Add CSS module mapper for style imports in tests

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   collectCoverageFrom: [
     'src/**/*.ts',
+    'src/**/*.tsx',
     '!src/**/*.d.ts',
     '!src/**/__tests__/**',
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 };
